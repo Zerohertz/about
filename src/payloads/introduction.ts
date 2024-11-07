00b9b234cf4629af9f6c7a8b22f4ffb590cdb5ee
Zerohertz <ohg3417@gmail.com>
2024-11-08 02:01:59 +0900

    :hammer: fix: github actions

--- src/payloads/introduction.ts
+++ src/payloads/introduction.ts
@@ -14,7 +14,7 @@ const introduction: Payload = {
     "I integrated project source codes using Git Submodule to create a seamless collaborative environment. I also introduced MLOps tools like DVC and MLflow into the team's workflow, greatly improving collaboration on data and models and fostering a productive team atmosphere.",
   ],
   signature: "Zerohertz",
-  latestUpdated: "2024-11-05",
+  latestUpdated: "2024-11-08",
 };
 
 export default introduction;

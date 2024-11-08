366d24c61cfd52f2cdc33ad0afabe985a8b4d6cf
Zerohertz <ohg3417@gmail.com>
2024-11-08 10:20:03 +0900

    :memo: modify: generic type

--- src/components/default/ComponentWrapper.tsx
+++ src/components/default/ComponentWrapper.tsx
@@ -2,14 +2,14 @@ import { FC } from "react";
 
 import Payload from "@/components/default/Payload";
 
-type Component<T> = (props: { payload: T; title?: string }) => JSX.Element;
+type Component<T> = (props: { payload: T }) => JSX.Element;
 
 const ComponentWrapper = <T extends Payload>(WrappedComponent: Component<T>): FC<{ payload: T; title?: string }> => {
-  const Wrapper: FC<{ payload: T; title?: string }> = ({ payload, title }) => {
+  const Wrapper: FC<{ payload: T }> = ({ payload }) => {
     if (payload?.disable) {
       return null;
     }
-    return <WrappedComponent payload={payload} title={title} />;
+    return <WrappedComponent payload={payload} />;
   };
   return Wrapper;
 };

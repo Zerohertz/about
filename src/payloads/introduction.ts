5c734e2fdb6cb7808a069228926978534aa432e6
Zerohertz <ohg3417@gmail.com>
2024-11-12 16:12:11 +0900

    :memo: docs: introduction

--- src/payloads/introduction.ts
+++ src/payloads/introduction.ts
@@ -4,13 +4,13 @@ const introduction: Payload = {
   title: "INTRODUCTION",
   disable: false,
   contents: [
-    "🌱 Continuously Growing Engineer",
-    "With a passion for learning and growth, I have been running a technical blog since 2018 on topics like Kubernetes, PyTorch, and Airflow, achieving 1,500+ monthly active users (MAU). I have participated in 10+ study groups, enjoying exploring the core principles of technology and applying them to solve real-world challenges.",
+    "🌱 **Continuously Growing Engineer**",
+    "With a passion for learning and growth, I have been running [a technical blog](https://zerohertz.github.io/) since 2018 on topics like Kubernetes, PyTorch, and Airflow, achieving 1,500+ monthly active users (MAU). I have participated in [10+ study groups](https://github.com/stars/Zerohertz/lists/99-boaz), enjoying exploring the core principles of technology and applying them to solve real-world challenges.",
 
-    "🚀 Efficiency-Maximizing Engineer",
+    "🚀 **Efficiency-Maximizing Engineer**",
     "I automate inefficient repetitive tasks and seek efficient methods for operational improvement. I developed a Python backend for model serving and optimized pipelines to enhance efficiency. By utilizing GitLab CI and Kubernetes, I built pipelines and deployed using the sidecar pattern, resolving version control issues and significantly reducing build times.",
 
-    "🫂 Collaboration-Focused Engineer",
+    "🫂 **Collaboration-Focused Engineer**",
     "I integrated project source codes using Git Submodule to create a seamless collaborative environment. I also introduced MLOps tools like DVC and MLflow into the team's workflow, greatly improving collaboration on data and models and fostering a productive team atmosphere.",
   ],
   signature: "Zerohertz",

13ee57c0e12cf33a34cad907b0cf703a16d5e158
Zerohertz <ohg3417@gmail.com>
2024-11-14 13:52:27 +0900

    :recycle: refactor: analytics to header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,44 +16,44 @@ function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>{global.headTitle}</title>
         <link rel="shortcut icon" href={global.favicon} />
-      </Head>
-      {/* Google Analytics */}
-      {global.analytics && global.analytics.google && (
-        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-ZCW0CR8M8X" />
-      )}
-      {global.analytics && global.analytics.google && (
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
+        {/* Google Analytics */}
+        {global.analytics && global.analytics.google && (
+          <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-ZCW0CR8M8X" />
+        )}
+        {global.analytics && global.analytics.google && (
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', '${global.analytics.google}');
             `,
-          }}
-        />
-      )}
-      {/* Naver Analytics */}
-      {global.analytics && global.analytics.naver && (
-        <Script strategy="afterInteractive" src="//wcs.naver.net/wcslog.js" />
-      )}
-      {global.analytics && global.analytics.naver && (
-        <Script
-          id="naver-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
+            }}
+          />
+        )}
+        {/* Naver Analytics */}
+        {global.analytics && global.analytics.naver && (
+          <Script strategy="afterInteractive" src="//wcs.naver.net/wcslog.js" />
+        )}
+        {global.analytics && global.analytics.naver && (
+          <Script
+            id="naver-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
             if(!wcs_add) var wcs_add = {};
             wcs_add["wa"] = "${global.analytics.naver}";
             if(window.wcs) {
               wcs_do();
             }
           `,
-          }}
-        />
-      )}
+            }}
+          />
+        )}
+      </Head>
       <Component {...pageProps} />
     </>
   );

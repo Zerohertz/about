e70f95d35592d2aec3a1d6ff1609976edd140146
Zerohertz <ohg3417@gmail.com>
2024-11-21 23:37:07 +0900

    :memo: docs: publications

--- src/payloads/publications.ts
+++ src/payloads/publications.ts
@@ -13,7 +13,7 @@ const publications: Payload = {
             "[Patent] [Apparatus and method for predicting coating layer thickness in slot-die coating process](https://patents.google.com/patent/KR20230120780A)",
           descriptions: [
             {
-              content: "Inventor: Changwoo Lee, Minho Jo, Yoonjae Lee, Hyogeun Oh",
+              content: "Inventor: Changwoo Lee, Minho Jo, Yoonjae Lee, **Hyogeun Oh**",
             },
             {
               content: "Application number: 1020220017419 (2022.02.10)",
@@ -34,7 +34,7 @@ const publications: Payload = {
             "[SCI(E)] [Classification and Redundancy Quantitative Evaluation Algorithm for Highly Efficient Fault Diagnosis of Rotary Machines in Roll-to-Roll systems](https://www.sciencedirect.com/science/article/pii/S0263224122014889)",
           descriptions: [
             {
-              content: "Author: Hyogeun Oh, Jaehyun Noh, Changbeom Joo, Gyoujin Cho, Jeongdai Jo, Changwoo Lee",
+              content: "Author: **Hyogeun Oh**, Jaehyun Noh, Changbeom Joo, Gyoujin Cho, Jeongdai Jo, Changwoo Lee",
             },
             {
               content: "Journal: Measurement [Impact Factor 5.60, JCR Top 17.19%]",
@@ -54,7 +54,7 @@ const publications: Payload = {
             "[SCI(E)] [Feature Selection Algorithm Based on Density and Distance for Fault Diagnosis Applied to a Roll-to-Roll Manufacturing System](https://academic.oup.com/jcde/article/9/2/805/6573580)",
           descriptions: [
             {
-              content: "Author: Hyogeun Oh, Yoonjae Lee, Jongsu Lee, Changbeom Joo, Changwoo Lee",
+              content: "Author: **Hyogeun Oh**, Yoonjae Lee, Jongsu Lee, Changbeom Joo, Changwoo Lee",
             },
             {
               content: "Journal: Journal of Computational Design and Engineering [Impact Factor 6.16, JCR Top 10.87%]",

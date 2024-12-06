6f8df662e4a9d643856f287b896fc908e8b399bc
Zerohertz <ohg3417@gmail.com>
2024-12-06 16:05:51 +0900

    :memo: docs: extras

--- src/payloads/extras.ts
+++ src/payloads/extras.ts
@@ -8,14 +8,14 @@ const extras: Payload = {
       title: "CKA (Certified Kubernetes Administrator)",
       href: "https://www.credly.com/badges/8b2942ea-3d31-483b-a64a-6a6e0a4f6a5f/linked_in_profile",
       subTitle: "The Linux Foundation",
-      startedAt: "2024-05",
-      endedAt: "2026-05",
+      startedAt: "2024-05-05",
+      endedAt: "2026-05-04",
     },
     {
       title: "전문연구요원",
       subTitle: "AgileSoDA, Tmax WAPL",
-      startedAt: "2023-02",
-      endedAt: "2026-02",
+      startedAt: "2023-02-28",
+      endedAt: "2026-02-27",
     },
     {
       title: "BOAZ 19th (Big Data Club)",
@@ -27,6 +27,16 @@ const extras: Payload = {
         {
           content: "Study & Project (GitHub)",
           href: "https://github.com/stars/Zerohertz/lists/99-boaz",
+        },
+      ],
+    },
+    {
+      title: "Baekjoon Online Judge",
+      href: "https://www.acmicpc.net/user/zerohertz",
+      descriptions: [
+        {
+          content: "Top 3.39% (Platinum V)",
+          href: "https://solved.ac/profile/zerohertz",
         },
       ],
     },

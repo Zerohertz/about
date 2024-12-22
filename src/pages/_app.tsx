34cd65b0d782c65c71ce25924871bd433f328b9b
Zerohertz <ohg3417@gmail.com>
2024-12-23 03:13:26 +0900

    :bug: fix: analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,45 +16,37 @@ function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>{global.headTitle}</title>
         <link rel="shortcut icon" href={global.favicon} />
-        {/* Google Analytics */}
-        {global.analytics && global.analytics.google && (
-          <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-ZCW0CR8M8X" />
-        )}
-        {global.analytics && global.analytics.google && (
-          <Script
-            id="google-analytics"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${global.analytics.google}');
-            `,
-            }}
-          />
-        )}
-        {/* Naver Analytics */}
-        {global.analytics && global.analytics.naver && (
-          <Script strategy="afterInteractive" src="//wcs.naver.net/wcslog.js" />
-        )}
-        {global.analytics && global.analytics.naver && (
-          <Script
-            id="naver-analytics"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
+      </Head>
+      <Component {...pageProps} />
+      {/* Google Analytics */}
+      {global.analytics && global.analytics.google && (
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-ZCW0CR8M8X" />
+      )}
+      {global.analytics && global.analytics.google && (
+        <Script strategy="afterInteractive" id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${global.analytics.google}');
+          `}
+        </Script>
+      )}
+      {/* Naver Analytics */}
+      {global.analytics && global.analytics.naver && (
+        <Script strategy="afterInteractive" src="//wcs.naver.net/wcslog.js" type="text/javascript" />
+      )}
+      {global.analytics && global.analytics.naver && (
+        <Script strategy="afterInteractive" id="naver-analytics" type="text/javascript">
+          {`
             if(!wcs_add) var wcs_add = {};
             wcs_add["wa"] = "${global.analytics.naver}";
             if(window.wcs) {
               wcs_do();
             }
-          `,
-            }}
-          />
-        )}
-      </Head>
-      <Component {...pageProps} />
+          `}
+        </Script>
+      )}
     </>
   );
 }

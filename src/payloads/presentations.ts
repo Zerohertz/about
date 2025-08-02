aec1bf6a271a5e5c34053428f7d3db20e36e8fd5
Zerohertz <ohg3417@gmail.com>
2025-08-02 23:15:32 +0900

    :memo: docs: presentations
    Signed-off-by: Zerohertz <ohg3417@gmail.com>

--- src/payloads/presentations.ts
+++ src/payloads/presentations.ts
@@ -11,7 +11,7 @@ const presentations: Payload = {
       startedAt: "2025-08-02",
       onGoing: false,
       descriptions: [
-        { content: "KR title: 오픈소스 LLM, vLLM으로 Production까지" },
+        { content: "오픈소스 LLM, vLLM으로 Production까지" },
         {
           content: "GitHub",
           href: "https://github.com/Zerohertz/Instruct_KR_2025_Summer_Meetup_vLLM",

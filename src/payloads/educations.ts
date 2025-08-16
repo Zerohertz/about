e86c69396fb46f458a07f7db26747f20b2ac7e89
Zerohertz <ohg3417@gmail.com>
2025-08-17 01:31:55 +0900

    :hammer: refactor(wip): educations
    Signed-off-by: Zerohertz <ohg3417@gmail.com>

--- src/payloads/educations.ts
+++ src/payloads/educations.ts
@@ -6,7 +6,7 @@ const educations: Payload = {
   list: [
     {
       title: {
-        ko: "건국대학교, 서울, 대한민국",
+        ko: "건국대학교, 서울",
         en: "Konkuk University, Seoul, Korea",
       },
       href: "https://grad.konkuk.ac.kr/",
@@ -36,18 +36,107 @@ const educations: Payload = {
     },
     {
       title: {
-        ko: "건국대학교, 서울, 대한민국",
+        ko: "SiM Lab. (스마트 지능형 제조시스템 연구실)",
+        en: "SiM Lab. (Smart intelligent Manufacturing system Laboratory)",
+      },
+      href: "https://sites.google.com/view/simlabkonkuk",
+      subTitle: "Research Student",
+      startedAt: "2021-03",
+      endedAt: "2023-02",
+      keywords: ["MATLAB", "Python", "PyTorch", "C++", "LabVIEW", "Ansys"],
+      descriptions: [
+        {
+          content: {
+            ko: "산업 규모 제조 시스템에서 공정 조건의 실시간 모니터링을 위해 시계열 및 비전 데이터를 활용한 진단 모델을 연구하고 개발했습니다.",
+            en: "Researched and developed diagnostic models utilizing time-series and vision data for real-time monitoring of process conditions in industrial-scale manufacturing systems.",
+          },
+        },
+        {
+          content: {
+            ko: "특징 선택 알고리즘에 관한 SCI(E) Q1급 연구 논문 2편을 발표하여 산업 응용 분야에서 효율적인 머신러닝 방법론을 발전시켰습니다.",
+            en: "Published two SCI(E) Q1-ranked research papers on feature selection algorithms, advancing efficient machine learning methodologies in industrial applications.",
+          },
+        },
+        {
+          content: {
+            ko: "10개 이상의 정부 및 산업 지원 프로젝트를 수행하여 머신러닝 분야의 연구 역량을 더욱 향상시켰습니다.",
+            en: "Executed 10+ government and industry-funded projects, further enhancing research capabilities in machine learning.",
+          },
+        },
+        // { content: "Developed a graphical user interface (GUI) for real-time monitoring." },
+        // {
+        //   content:
+        //     "Performed computational fluid dynamics (CFD) simulations based on tension and temperature in roll-to-roll continuous processes.",
+        // },
+      ],
+    },
+    {
+      title: {
+        ko: "건국대학교, 서울",
         en: "Konkuk University, Seoul, Korea",
       },
       href: "https://www.konkuk.ac.kr/",
       subTitle: "B.S. in Department of Mechanical Engineering",
       startedAt: "2017-03",
       endedAt: "2021-02",
-      // descriptions: [
-      //   { content: 'Thesis: Smart Data-Based 3D Printer Fault Diagnosis System Through FDR' },
-      //   { content: 'Advisor: Changwoo Lee' },
-      //   { content: 'GPA: 3.72 / 4.5' },
-      // ],
+      descriptions: [
+        {
+          content: {
+            en: "Thesis: Smart Data-Based 3D Printer Fault Diagnosis System Through FDR",
+            ko: "Thesis: Smart Data-Based 3D Printer Fault Diagnosis System Through FDR",
+          },
+        },
+        {
+          content: {
+            ko: "Advisor: Changwoo Lee",
+            en: "Advisor: Changwoo Lee",
+          },
+        },
+        {
+          content: {
+            ko: "GPA: 3.72 / 4.5",
+            en: "GPA: 3.72 / 4.5",
+          },
+        },
+      ],
+    },
+    {
+      title: {
+        ko: "SiM Lab. (스마트 지능형 제조시스템 연구실)",
+        en: "SiM Lab. (Smart intelligent Manufacturing system Laboratory)",
+      },
+      href: "https://sites.google.com/view/simlabkonkuk",
+      subTitle: "Research Intern",
+      startedAt: "2019-11",
+      endedAt: "2021-02",
+      keywords: ["MATLAB", "Python", "PyTorch", "LabVIEW", "Ansys"],
+      descriptions: [
+        {
+          content: {
+            ko: "베어링 상태 진단 모델과 그래픽 사용자 인터페이스를 개발했습니다.",
+            en: "Developed bearing condition diagnostic model and graphic user interface.",
+          },
+        },
+      ],
+    },
+    {
+      title: {
+        ko: "MRV Lab. (의료 로보틱스 및 가상현실 연구실)",
+        en: "MRV Lab. (Medical Robotics and Virtual Reality Laboratory)",
+      },
+      href: "https://rvlab.konkuk.ac.kr/",
+      subTitle: "Research Intern",
+      startedAt: "2018-06",
+      endedAt: "2019-11",
+      keywords: ["Unreal Engine", "C++", "Python"],
+      descriptions: [
+        {
+          content: {
+            ko: "C++와 Unreal Engine을 기반으로 가상현실 환경을 개발했습니다.",
+            en: "Developed virtual reality environments based on C++ and Unreal Engine.",
+          },
+        },
+      ],
     },
   ],
 };

a1d32e788efa26e96ea504b888a8317eb83b176f
Zerohertz <ohg3417@gmail.com>
2025-08-20 01:03:12 +0900

    :memo: docs: presentations
    Signed-off-by: Zerohertz <ohg3417@gmail.com>

--- src/payloads/presentations.ts
+++ src/payloads/presentations.ts
@@ -6,8 +6,8 @@ const presentations: Payload = {
   list: [
     {
       title: {
-        ko: "vLLM의 OpenAI-Compatible Server 톺아보기",
         en: "Exploring vLLM's OpenAI-Compatible Server",
+        ko: "vLLM의 OpenAI-Compatible Server 톺아보기",
       },
       href: "https://2025.pycon.kr/presentations/b00a81cd-4ea3-4ec6-a658-d5e7f54c6d3f",
       subTitle: "PyCon Korea 2025 Tutorial",
@@ -16,15 +16,15 @@ const presentations: Payload = {
       descriptions: [
         {
           content: {
-            ko: "GitHub",
             en: "GitHub",
+            ko: "GitHub",
           },
           href: "https://github.com/Zerohertz/PyCon_KR_2025_Tutorial_vLLM",
         },
         {
           content: {
-            ko: "SlideShare",
             en: "SlideShare",
+            ko: "SlideShare",
           },
           href: "https://www.slideshare.net/slideshow/exploring-vllm-s-openai-compatible-server-pycon-korea-tutorial-2025/282444625",
         },
@@ -32,8 +32,8 @@ const presentations: Payload = {
     },
     {
       title: {
-        ko: "오픈 소스 LLM, vLLM에서 프로덕션까지",
         en: "Open Source LLMs, from vLLM to Production",
+        ko: "오픈 소스 LLM, vLLM에서 프로덕션까지",
       },
       href: "https://lu.ma/lahqslya",
       subTitle: "Instruct.KR Summer 2025 Meetup",
@@ -42,15 +42,15 @@ const presentations: Payload = {
       descriptions: [
         {
           content: {
-            ko: "GitHub",
             en: "GitHub",
+            ko: "GitHub",
           },
           href: "https://github.com/Zerohertz/Instruct_KR_2025_Summer_Meetup_vLLM",
         },
         {
           content: {
-            ko: "SlideShare",
             en: "SlideShare",
+            ko: "SlideShare",
           },
           href: "https://www.slideshare.net/slideshow/llm-vllm-production-instruct-kr-summer-meetup-2025-9d16/282169536",
         },
